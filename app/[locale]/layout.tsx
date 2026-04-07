15edf59ec4630399b1d259169d2e6188ffb63b81
sailarn <denis9524@gmail.com>
2026-04-07 15:23:41 +0300

    chore: fix biome

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -5,8 +5,8 @@ import { notFound } from "next/navigation";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { ClientShell } from "@/components/client-shell";
-import { ThemeColorSync } from "@/components/theme-color-sync";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ThemeColorSync } from "@/components/theme-color-sync";
 import { locales } from "@/i18n/request";
 import "../globals.css";
 
